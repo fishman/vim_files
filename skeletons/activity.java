1d0d4258dd61c127273ee51894bf7e89567a6717
Reza Jelveh <toyb0mb@gmx.net>
2012-08-06 16:52:13 +0100

    add activity

---
+++ skeletons/activity.java
@@ -0,0 +1,14 @@
+package com.hello.world;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.*;
+
+public class HelloworldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
